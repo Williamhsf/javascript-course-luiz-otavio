14b0886fb6112d75949f21be418d80caa3fbecf8
Williamhsf <williamhenriquefb@gmail.com>
2024-01-16 19:24:14 -0300

    subindo repositorio

---
+++ secao4-JavaScript-funcoes(avancado)/03-retorno-da-funcao/teste/main.js
@@ -0,0 +1,10 @@
+// funcoes que retornam algum valor
+function soma(a, b) {
+    return a + b; 
+}
+
+// funcoes que retornam algo diferente
+// no caso troca o fundo da tela (vermelho)
+document.addEventListener('click', function () {
+    document.body.style.backgroundColor = 'red';
+});
