5f6149f361cbe49ac8080aa14a95a9946e11a602
Williamhsf <williamhenriquefb@gmail.com>
2024-02-05 23:51:55 -0300

    subindo repositorio

--- secao4-JavaScript-funcoes(avancado)/09-funcoes-construtora/main.js
+++ secao4-JavaScript-funcoes(avancado)/09-funcoes-construtora/main.js
@@ -2,14 +2,18 @@
 // Funcao fabrica - fabrica objetos
 // construtora -> sempre inicia com letra maiuscula Pessoa (new)
 
-
-
 function Pessoa(nome, sobrenome) {
+  // Atributos ou Metodos Publicos
   this.nome = nome;
   this.sobrenome = sobrenome;
+
+  this.metodo = function() {
+    console.log(this.nome + ': sou um metodo');
+  };
 }
 
-const p1 = new Pessoa('Luiz', 'otavio');
-console.log(p1.nome)
-
-const p2 = new Pessoa('Maria', 'otavio');
+// new cria o objeto vazio e usa o this para retornar ao mesmo
+const p1 = new Pessoa('Luiz', 'Otavio');
+const p2 = new Pessoa('Maria', 'Oliveira');
+p1.metodo();
+p2.metodo();
